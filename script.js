819573fa062d5eee72019b7b3199e5adcac74887
Rebeca Ruiz <rebeca.ruiz.guardia0102@gmail.com>
2024-11-04 15:41:05 -0500

    Agregar menu desplegable para mobile y tablet

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
+const menu = document.querySelector('.navegacion');
+const abrirMenu = document.querySelector('.menu_icon');
+const cerrarMenu = document.querySelector('.navegacion__icon');
+
+abrirMenu.addEventListener('click', () => {
+    menu.classList.add('visible');
+});
+
+cerrarMenu.addEventListener('click', () => {
+    menu.classList.remove('visible');
+});
